d1c9c4b04e762e9a277a76f6079482bda320b43c
ddorfman77 <david@yapicentral.com>
2017-05-09 19:36:15 -0700

    Add files via upload

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,6 +1,33 @@
+
+
 $("#button").on("click", function(){
   event.preventDefault();
   $("#search-box").addClass("move");
   $("#sectiontwo").css("display", "block")
-  $(".logo").css("display", "none")
+  $(".logo").css({"height":"auto" , "width":"200px"})
 })
+
+$.ajax({
+  type:"GET",
+  url:"https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=Qfv1suz3Z2XxzUENmXjYxN59XsuJPA2j",
+  async:true,
+  dataType: "json",
+  success: function(json) {
+              console.log(json);
+              // Parse the response.
+              // Do other things.
+           },
+  error: function(xhr, status, err) {
+              // This time, we do not end up here!
+           }
+});
+
+
+//pop up 
+$('#my_popup').popup({
+  color: 'white',
+  opacity: 1,
+  transition: '0.3s',
+  scrolllock: true
+});
+
