b5da89401dad2ef80ac707c92ad2aaf5808b8438
Carlos Perez <solracias@gmail.com>
2017-05-13 03:25:29 -0700

    can now search by name which is case sensitive. Can also search by radius. Set up to 50 miles for now

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -8,6 +8,15 @@ $("#button").on("click", function(){
   seatGeek();
 })
 
+$(".radiusButton").on("click", function(){
+  event.preventDefault();
+  $("#search-box").addClass("move");
+  $("#sectiontwo").css("display", "block")
+  $(".logo").css({"height":"auto" , "width":"200px"})
+
+  seatGeek2();
+})
+
 //pop up 
 $('#my_popup').popup({
   color: 'white',
@@ -17,8 +26,11 @@ $('#my_popup').popup({
 });
 
 function seatGeek(){
-  var queryURL = "https://api.seatgeek.com/2/events?per_page=30&page=10&geoip=true&range=10mi&client_id=NzU0MjI0N3wxNDk0MzgxMTAzLjcy";
-  var bandURL = "https://api.seatgeek.com/2/performers?q=red+hot&client_id=NzU0MjI0N3wxNDk0MzgxMTAzLjcy";
+  var performer = $("#search-input").val().trim();
+  var miles = $("#miles").val().trim();
+  parseInt(miles)
+  var queryURL = "https://api.seatgeek.com/2/events?per_page=30&page=10&geoip=true&range="+miles+"mi&client_id=NzU0MjI0N3wxNDk0MzgxMTAzLjcy";
+  var bandURL = "https://api.seatgeek.com/2/performers?q="+performer+"&client_id=NzU0MjI0N3wxNDk0MzgxMTAzLjcy";
   $.when(
     $.ajax({
       url: bandURL,
@@ -29,14 +41,15 @@ function seatGeek(){
       method: "GET"
     })
   ).done(function(band, response){
-    // console.log(band[0].performers);
+    // console.log(band);
     // console.log(response[0].events);
-
+    // Dierks Bentley
     // creates an array filtering only for concerts
     var concert = [];
     for (var i = 0; i < response[0].events.length; i++){
-      if (response[0].events[i].type === "concert"){
+      if (response[0].events[i].type === "concert" && response[0].events[i].performers[0].name == performer){
         concert.push(response[0].events[i]);
+        // console.log(concert);
       }
     }
 
@@ -50,6 +63,46 @@ function seatGeek(){
   })
 }
 
+function seatGeek2(){
+  var performer = $("#search-input").val().trim();
+  var miles = $("#miles").val().trim();
+  parseInt(miles)
+  var queryURL = "https://api.seatgeek.com/2/events?per_page=30&page=10&geoip=true&range="+miles+"mi&client_id=NzU0MjI0N3wxNDk0MzgxMTAzLjcy";
+  var bandURL = "https://api.seatgeek.com/2/performers?q="+performer+"&client_id=NzU0MjI0N3wxNDk0MzgxMTAzLjcy";
+  $.when(
+    $.ajax({
+      url: bandURL,
+      method: "GET"
+    }),
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+  ).done(function(band, response){
+    // console.log(band);
+    // console.log(response[0].events);
+    // Dierks Bentley
+    // creates an array filtering only for concerts
+
+    var concert = [];
+    for (var i = 0; i < response[0].events.length; i++){
+      if (response[0].events[i].type === "concert"){
+        concert.push(response[0].events[i]);
+        // console.log(concert);
+      }
+    }
+
+    //displays only concerts to html table
+    for (var i = 0; i < concert.length; i++){
+      $("#table").append('<tr><td>' + concert[i].performers[0].name+
+      '</td><td>' +concert[i].venue.name +
+      '</td><td>' +concert[i].stats.lowest_price +
+      '</td><td><a href="' +concert[i].url+ '">SeatGeek</a></td>');
+    }
+  })
+}
+
+ // && response[0].events[i].performers[0].name === band[0].performers[0]
 
 
 
